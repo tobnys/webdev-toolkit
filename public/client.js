72a6d0466144d3ce69b60822fbc0790b905a43b9
tobnys <tobnystrom@gmail.com>
2017-10-10 17:05:45 +0200

    Main HTML work completed

--- public/client.js
+++ public/client.js
@@ -1,33 +1,35 @@
-$(".toggle-container").on("click", function(e){
-    let value = $(".main-nav").css("height");
-    if(value === "50px") {
-        $(".main-nav").animate({
-            height: "300px"
-        })
-        $(".inline-nav-container a").animate({
-            opacity: "1"
-        })
-        $(".main-nav").css("background-color", "rgba(0, 119, 131, 1)")
-        $(".main-nav a").css("display", "block")
-    }
-    else {
-        $(".main-nav").animate({
-            height: "50px"
-        })
-        $(".inline-nav-container a").animate({
-            opacity: "0"
-        })
-        $(".main-nav").css("background-color", "rgba(0, 119, 131, 0.2)")
-        $(".main-nav a").css("display", "none")
-    }
-});
-
-$("#dropdown").on("click", function(e){
-    let value = $(".hidden").css("display");
-    if(value === "block") {
-        $(".hidden").css("display", "none");
-    }
-    else {
-        $(".hidden").css("display", "block");
-    }
-});
+(function () {
+    $(".toggle-container").on("click", function(e){
+        let value = $(".main-nav").css("height");
+        if(value === "50px") {
+            $(".main-nav").animate({
+                height: "300px"
+            })
+            $(".inline-nav-container a").animate({
+                opacity: "1"
+            })
+            $(".main-nav").css("background-color", "rgba(0, 119, 131, 1)")
+            $(".main-nav a").css("display", "block")
+        }
+        else {
+            $(".main-nav").animate({
+                height: "50px"
+            })
+            $(".inline-nav-container a").animate({
+                opacity: "0"
+            })
+            $(".main-nav").css("background-color", "rgba(0, 119, 131, 0.2)")
+            $(".main-nav a").css("display", "none")
+        }
+    });
+    
+    $("#dropdown").on("click", function(e){
+        let value = $(".hidden").css("display");
+        if(value === "block") {
+            $(".hidden").css("display", "none");
+        }
+        else {
+            $(".hidden").css("display", "block");
+        }
+    });
+})();
